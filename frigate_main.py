604320163e16d28030e82c4f8ae47d9248a06d01
whiteted <whiteted.strats@gmail.com>
2020-10-19 23:34:50 +0100

    initial stairs marking

--- frigate_main.py
+++ frigate_main.py
@@ -45,6 +45,66 @@ def saveFig(plt, fig, path):
     fig.tight_layout(pad=0)
     plt.savefig(path, bbox_inches='tight', pad_inches=0, dpi=254)   # 254 is 1 pixel per cm in GE world
 
+def connectedComponents(tileAddrs, tiles):
+    stack = []
+    comps = []
+    seen = set([0]) # null tile
+    universe = set(tileAddrs)
+
+    i = 0
+
+    while i < len(tileAddrs):
+        tileAddr = tileAddrs[i]
+        if tileAddr in seen:
+            i += 1
+        else:
+            # New tileAddr
+            stack = [tileAddr]
+            seen.add(tileAddr)
+            currComp = []
+
+            while len(stack) > 0:
+                tileAddr = stack.pop()
+                currComp.append(tileAddr)
+
+                for link in tiles[tileAddr]["links"]:
+                    # Only ever add it to the link once
+                    # Restrict to just the tiles we're interested in
+                    if link not in seen and link in universe:
+                        seen.add(link)
+                        stack.append(link)
+            
+            comps.append(currComp)
+    
+    return comps
+
+
+def markStairs(tilePlanes, tiles, plt):
+    for (n,a), tileAddrs in tilePlanes.items():
+        if n[1] != 0:
+            continue
+        
+        comps = connectedComponents(tileAddrs, tiles)
+
+        # Plane is vertical, so it's just a series of line segments from above
+        # Group into connected components then compare
+
+        # Search for the two most extreme points
+        # This works because if x varies on the line, we are just comparing by x
+        # Otherwise we are just comparing by z
+
+        for comp in comps:
+            min_p = max_p = tiles[comp[0]]["points"][0]
+            for tileAddr in comp:
+                td = tiles[tileAddr]
+                for p in td["points"]:
+                    min_p = min(min_p, p)
+                    max_p = max(max_p, p)
+            
+            xs, zs = zip(min_p, max_p)
+            plt.plot([-x for x in xs], zs, linewidth=0.5, color='b')
+            
+
 def main(plt, tiles, dividingTiles, startTileName, objects, level_scale, GROUP_NO, path):
     # Global (above a specific group) preperations
     prepTiles(tiles)
@@ -61,6 +121,9 @@ def main(plt, tiles, dividingTiles, startTileName, objects, level_scale, GROUP_N
     drawTileHardEdges(currentTiles, tiles, (0.65, 0.65, 0.65), axs)
     drawGuards(guards, currentTiles, axs)
     drawObjects(plt, axs, objects, tiles, currentTiles)
+
+    # Testing
+    markStairs(tilePlanes, tiles, plt)
 
     # Call frig specific code
     frig_specific(tilePlanes, tiles, plt, axs)
